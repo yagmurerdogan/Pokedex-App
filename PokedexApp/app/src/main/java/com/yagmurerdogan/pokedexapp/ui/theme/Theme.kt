697ebf9bed95314f13aeb3f0463e975b43dbfdf3
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-07-01 09:26:58 +0300

    Change theme colors

--- PokedexApp/app/src/main/java/com/yagmurerdogan/pokedexapp/ui/theme/Theme.kt
+++ PokedexApp/app/src/main/java/com/yagmurerdogan/pokedexapp/ui/theme/Theme.kt
@@ -5,17 +5,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color.Yellow,
+    background = Color(0xFF101010),
+    onBackground = Color.White,
+    surface = Color(0xFF303030),
+    onSurface = Color.White
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color.Blue,
+    background = LightBlue,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Color.Black
 
     /* Other default colors to override
     background = Color.White,
