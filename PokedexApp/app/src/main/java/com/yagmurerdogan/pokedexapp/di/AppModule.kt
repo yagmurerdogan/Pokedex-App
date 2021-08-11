57f767de77e7b60f43bdc4e7d070e566a93a1a67
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-11 12:38:34 +0300

    Create app module

--- PokedexApp/app/src/main/java/com/yagmurerdogan/pokedexapp/di/AppModule.kt
+++ PokedexApp/app/src/main/java/com/yagmurerdogan/pokedexapp/di/AppModule.kt
@@ -1,11 +1,33 @@
 package com.yagmurerdogan.pokedexapp.di
 
+import com.yagmurerdogan.pokedexapp.data.remote.PokeApi
+import com.yagmurerdogan.pokedexapp.repository.PokemonRepository
+import com.yagmurerdogan.pokedexapp.util.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Singleton
+    @Provides
+    fun providePokemonRepository(
+        api: PokeApi
+    ) = PokemonRepository(api)
+
+    @Singleton
+    @Provides
+    fun providePokeApi(): PokeApi {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PokeApi::class.java)
+    }
 }
