627ba2a5cd062ed230aa0c9e1c56083eff23c372
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-11 12:18:11 +0300

    Create PokeApi interface

---
+++ PokedexApp/app/src/main/java/com/yagmurerdogan/pokedexapp/data/remote/PokeApi.kt
@@ -0,0 +1,21 @@
+package com.yagmurerdogan.pokedexapp.data.remote
+
+import com.yagmurerdogan.pokedexapp.data.remote.responses.Pokemon
+import com.yagmurerdogan.pokedexapp.data.remote.responses.PokemonList
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface PokeApi {
+
+    @GET("pokemon")
+    suspend fun getPokemonList(
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int
+    ): PokemonList
+
+    @GET("pokemon/{name}")
+    suspend fun getPokemonInfo(
+        @Path("name") name: String
+    ): Pokemon
+}
