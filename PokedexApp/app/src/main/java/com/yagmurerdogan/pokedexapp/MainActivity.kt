913cf964bb5f7d6c9dea14ffaf32974794487263
Yağmur Erdoğan <yagmur.erdogan.1999@gmail.com>
2021-08-13 09:39:55 +0300

    Create pokemon list screen file

--- PokedexApp/app/src/main/java/com/yagmurerdogan/pokedexapp/MainActivity.kt
+++ PokedexApp/app/src/main/java/com/yagmurerdogan/pokedexapp/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
+import com.yagmurerdogan.pokedexapp.pokemonlist.PokemonListScreen
 import com.yagmurerdogan.pokedexapp.ui.theme.PokedexAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -30,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     startDestination = "pokemon_list_screen"
                 ) {
                     composable("pokemon_list_screen") {
-
+                        PokemonListScreen(navController = navController)
                     }
                     composable(
                         "pokemon_detail_screen/{dominantColor}/{pokemonName}",
